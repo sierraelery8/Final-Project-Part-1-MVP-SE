2b8987fcdec024705854acc58bfe2c18797c216c
sierraelery8 <sierra.elery@centenaryuniversity.edu>
2026-04-18 19:48:38 -0400

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-require("dotenv").config();
-const express = require("express");
+// app.js
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
-const authRoutes = require("./routes/authRoutes");
-const plantRoutes = require("./routes/plantRoutes");
-const errorHandler = require("./middleware/errorHandler");
-
+// middleware
 app.use(express.json());
 
-// routes
-app.use("/auth", authRoutes);
-app.use("/plants", plantRoutes);
+// ROUTES
+const plantRoutes = require('./routes/plantRoutes');
+const userRoutes = require('./routes/userRoutes');
+const careLogRoutes = require('./routes/careLogRoutes');
 
-// error handler (must be last)
-app.use(errorHandler);
+app.use('/plants', plantRoutes);
+app.use('/users', userRoutes);
+app.use('/carelogs', careLogRoutes);
+
+// basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
+// global error handler (centralized)
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    error: err.message || 'Internal server error'
+  });
+});
 
 module.exports = app;
