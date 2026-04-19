099114aedf48ea8f8ffa9b3cdab461c4b075d57e
sierraelery8 <sierra.elery@centenaryuniversity.edu>
2026-04-18 23:06:20 -0400

    Implement JWT authentication and role-based authorization

--- config/database.js
+++ config/database.js
@@ -1,8 +1,25 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database/database.sqlite'
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  // use PostgreSQL on Render
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  // use SQLite locally
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database/database.sqlite"
+  });
+}
 
 module.exports = sequelize;
